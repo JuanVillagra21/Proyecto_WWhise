e0005026b6923e6720beeb70b0b2731b232936da
JuanVillagra <villagrajuan99@gmail.com>
2025-10-23 23:49:37 -0300

    Proyecto agrefado

---
+++ Proyecto_WWhise_V2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,52 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MUTE_AMBIENTE = 3700912920U;
+        static const AkUniqueID PLAY_AMBIENTE = 94063951U;
+        static const AkUniqueID PLAY_CAMA = 3250665892U;
+        static const AkUniqueID PLAY_CAMILLA = 3408996381U;
+        static const AkUniqueID PLAY_GOLPE_UNAVEZ = 1971000861U;
+        static const AkUniqueID PLAY_MUEBLE_V1 = 3360156480U;
+        static const AkUniqueID PLAY_MUEBLE_V2 = 3360156483U;
+        static const AkUniqueID PLAY_PUERTA_GOLPE = 3686802613U;
+        static const AkUniqueID PLAY_SUSTO = 545700022U;
+        static const AkUniqueID PLAY_SUSURRO_V2 = 1031156720U;
+        static const AkUniqueID PLAY_SUSURRO_V3 = 1031156721U;
+        static const AkUniqueID PLAY_TUBO_FLUORESECENTE = 2176072023U;
+        static const AkUniqueID PLAY_TUBO_FLUORESENTE_V2 = 3235161614U;
+        static const AkUniqueID PLAY_VENTANA = 1727156503U;
+        static const AkUniqueID UNMUTE_SUSURRO = 4152172729U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
